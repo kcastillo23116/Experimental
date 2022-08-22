2e1c512cdb5c149eebf242033e6c919346cf6b82
kcastillo23116 <kcastillo23116@gmail.com>
2022-08-22 15:19:18 -0400

    Adds more images for alternate banker stances, bank close button and wood to mitigate image not found issues

--- RunescapeScripts/Bankstanding/FletchBow.py
+++ RunescapeScripts/Bankstanding/FletchBow.py
@@ -4,6 +4,7 @@ client:             - RuneLite with resizeable modern layout in runescape settin
                       and RuneLite Stretched mode enabled with Resizable scaling set to 75%
                     - Runelite sidebar CLOSED
                     - Turn on object markers plugin and Shift+Right click bank to mark it
+                    - Turn off menu opacity in runecape options: Interfaces > resizable > Transparent side panel
 Monitors:           4k middle monitor with all three on
 bank settings:      Withdraw As: Item
                     Quantity: All
@@ -26,7 +27,11 @@ loopsTillDone = round(itemCount / 27)
 
 try:
     # Open bank to start
-    banker_images = ['Images/Banker.png', 'Images/Banker2.png', 'Images/Banker3.png']
+    banker_images = ['Images/Banker.png', 'Images/Banker2.png',
+                     'Images/Banker3.png', 'Images/Banker4.png',
+                     'Images/Banker5.png']
+    bank_close_images = ['Images/BankClose.png', 'Images/BankClose2.png']
+    wood_images = ['Images/Wood.png', 'Images/Wood2.png']
     Common.click_image(banker_images, 0.2, 1, 'Right click banker', True)
 
     Common.click_image(['Images/BankOption.png'], 0.7, 1, 'Open bank')
@@ -34,10 +39,10 @@ try:
     for x in range(loopsTillDone):
         Common.print_runtime(loopsTillDone, 55, x)
 
-        Common.click_image(['Images/Wood.png'], 0.5, 1, 'Get wood')
-        Common.click_image(['Images/BankClose.png'], 0.5, 1, 'Close bank')
+        Common.click_image(['Images/Wood2.png'], 0.7, 1, 'Get wood')
+        Common.click_image(bank_close_images, 0.9, 1, 'Close bank')
         Common.click_image(['Images/Knife.png'], 0.5, 1, 'Click knife')
-        Common.click_image(['Images/Wood.png'], 0.7, 1, 'Click inventory wood')
+        Common.click_image(wood_images, 0.7, 1, 'Click inventory wood')
         Common.click_image(['Images/LongBow.png'], 0.7, 1, 'Click longbow craft menu option')
         Common.click_image(banker_images, 0.2, 49, 'Right click banker', True)
         Common.click_image(['Images/BankOption.png'], 0.7, 1, 'Open bank')
