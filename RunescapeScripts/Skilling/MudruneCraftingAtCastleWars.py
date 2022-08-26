92a890dbbdc1df306173f97356ef96fcbfbe6d34
kcastillo23116 <kcastillo23116@gmail.com>
2022-08-26 17:37:38 -0400

    Updates mudrune crating timing per run for accuraccy

--- RunescapeScripts/Skilling/MudruneCraftingAtCastleWars.py
+++ RunescapeScripts/Skilling/MudruneCraftingAtCastleWars.py
@@ -103,7 +103,7 @@ try:
     Common.watch_click_image(bank_images, 0.7, 'Click bank chest', False, 0, 10, bank_close_images)
 
     for x in range(loopsTillDone):
-        Common.print_runtime(loopsTillDone, 140, x)
+        Common.print_runtime(loopsTillDone, 90, x)
 
         # region BANK STUFF
 
