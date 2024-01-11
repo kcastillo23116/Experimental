54eab2d13fc33e96972adf9d84a8581103a5515c
Gaming4\Kevin <kcastillo23116@gmail.com>
2024-01-11 02:10:07 -0500

    Adds tan leather bankstanding script

--- RunescapeScripts/Bankstanding/TanLeatherSpell.py
+++ RunescapeScripts/Bankstanding/TanLeatherSpell.py
@@ -14,9 +14,12 @@ bank settings:      - Withdraw As: Item
 location:           - Grand exchange, directly in front of the right bottom bank teller box
                     - click compass, so it's at default north and zoom in, hold up arrow to get camera all the way up
 Menus:              - Spellbook not open
-equip items:        - Steam staff
-items in inventory: - Astral runes
-items in bank:      - Raw pies
+equip items:        - Fire staff
+items in inventory: - Rune pouch with following runes:
+                        - Nature
+                        - Astral
+items in bank:      - Blue Dragon Hide
+                    - Red Dragon Hide
 Objects to Mark:    - With: NPC Indicator settings:
                         - Highlight Hull
                         - Highlight tile
@@ -37,22 +40,26 @@ import Display
 defaultTimeBeforeClick = 1
 
 # Images
-unbaked_pie_images = ['Images/Cooking/UnbakedPieBank.png']
-bake_pie_spell_images = ['Images/Cooking/BakePieSpell.png']
-baked_pie_images = ['Images/Cooking/BakedPie.png']
 lunar_spellbook_tab_images = ['Images/Cooking/LunarSpellbookTab.png']
+dragon_hide_images = ['Images/BankStanding/TanLeather/BlackDragonHide.png',
+                      'Images/BankStanding/TanLeather/BlueDragonHide.png',
+                      'Images/BankStanding/TanLeather/RedDragonHide.png']
+tan_leather_spell_images = ['Images/BankStanding/TanLeather/TanLeatherSpell.png']
+dragon_leather_images = ['Images/BankStanding/TanLeather/BlackDragonLeather.png',
+                         'Images/BankStanding/TanLeather/BlueDragonLeather.png',
+                         'Images/BankStanding/TanLeather/RedDragonLeather.png']
 
 
-def cast_bake_pies():
+def cast_tan_leather():
     try:
         # Get how many from user
-        item_count_string = input("How many pies do do want to bake? ")
+        item_count_string = input("How many hides do do want to tan? ")
         item_count = int(item_count_string)
 
         # Calculate how many loops needed
-        loops_till_done = round(item_count / 27)
+        loops_till_done = round(item_count / 25)
 
-        Common.watch_click_image(lunar_spellbook_tab_images, 0.7, 'Get raw pies', sleep_time_after_click=1,
+        Common.watch_click_image(lunar_spellbook_tab_images, 0.7, 'Open lunar spellbook', sleep_time_after_click=1,
                                  current_step_region=Common.Inventory_bar_region)
 
         for x in range(loops_till_done):
@@ -60,17 +67,19 @@ def cast_bake_pies():
 
             Banking.open_grand_exchange_bank()
 
-            Common.watch_click_image(unbaked_pie_images, 0.7, 'Get raw pies', sleep_time_after_click=1,
+            Common.watch_click_image(dragon_hide_images, 0.7, 'Get dragon hides', sleep_time_after_click=1,
                                      current_step_region=Common.Bank_region)
 
             Banking.close_bank()
 
-            Common.watch_click_image(bake_pie_spell_images, 0.7, 'Cast bake pie spell', sleep_time_after_click=49,
-                                     current_step_region=Common.Inventory_region)
+            # Cast tan leather five times to tan 25 hides
+            for y in range(5):
+                Common.watch_click_image(tan_leather_spell_images, 0.7, 'Cast tan leather ' + str(y),
+                                         sleep_time_after_click=2, current_step_region=Common.Inventory_region)
 
             Banking.open_grand_exchange_bank()
 
-            Common.watch_click_image(baked_pie_images, 0.7, 'Deposit cooked pies', sleep_time_after_click=1,
+            Common.watch_click_image(dragon_leather_images, 0.7, 'Deposit tanned leather', sleep_time_after_click=1,
                                      current_step_region=Common.Inventory_region)
 
             # Subtract iteration already ran and display countdown timer
@@ -83,5 +92,5 @@ def cast_bake_pies():
 
 # Only run code below if running this script
 if __name__ == '__main__':
-
-    cast_bake_pies()
+    cast_tan_leather()
+    print('Done tanning leather!')
