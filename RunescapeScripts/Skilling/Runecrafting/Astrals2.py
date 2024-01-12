9702f39b54cae699ddef51c475aa1baedf00982d
Gaming4\Kevin <kcastillo23116@gmail.com>
2024-01-12 16:40:23 -0500

    Updates Astral2 runecrafting script to use player house pool to restore stamina

--- RunescapeScripts/Skilling/Runecrafting/Astrals2.py
+++ RunescapeScripts/Skilling/Runecrafting/Astrals2.py
@@ -32,23 +32,29 @@ equip items:        - Dust battlestaff
 items in inventory: - Giant/Large/Medium/small Rune essences pouches
                     - Cosmic and Law runes in rune pouch
 items in bank:      - Rune essence
-                    - Stamina potions (4)
+                    - Teleport home tablets
                     - Abyssal Book so dark mage repairs with NPC Contact spell (Can be anywhere in bank)
 Setup:           - Open bank to tab with all items
                  - With bank window open, swap the left click for each rune pouch to Fill option
                  - Shift click NPC contact spell and set left click to Dark Mage via Runelite menu entry swapper plugin
                        - Note: May have to contact dark mage once before this is available
                  - Make sure inventory menu is not open
+                 - Change banked player house tablet left click to withdraw 1 using runelite menu swapper plugin
+                 - Change banked lobster left click to withdraw 1 using runelite menu swapper plugin
+                 - Disable Regeneration Meter runelite plugin so it doesn't get in the way of checking HP
                  - Mark following inventory items using Inventory Tags runelite plugin:
                     - Small pouch: Yellow
                     - Medium pouch: Red
                     - Large pouch: Teal
                     - Giant pouch: Magenta
-Objects to mark: - Enable highlight outline
+                    - Rune pouch: Blue (0043FF)
+                - Disable Rune Pouch plugin so runes in pouch are not shown
+Objects to mark: - Enable Highlight hull and Highlight clickbox
                  - Marker color to default magenta (FFD400FF)
                  - Border width: 6
                  - Middle bank teller
                  - Astral altar
+                 - Revitalisation pool or above in Player house for stamina recovery
 """
 from time import sleep
 
@@ -69,11 +75,11 @@ giant_rune_pouch_images = ['Images/Runecrafting/Astral/RunePouchGiant.png']
 large_rune_pouch_images = ['Images/Runecrafting/Astral/RunePouchLarge.png']
 med_rune_pouch_images = ['Images/Runecrafting/Astral/RunePouchMedium.png']
 small_rune_pouch_images = ['Images/Runecrafting/Astral/RunePouchSmall.png']
+rune_pouch_images = ['Images/Runecrafting/Astral/RunePouch.png']
 stamina_potion_bank = ['Images/Astral/StaminaPotion.png']
 stamina_potion_images = ['Images/Runecrafting/Astral/StaminaPotion.png',
                          'Images/Runecrafting/Astral/StaminaPotion2.png',
                          'Images/Runecrafting/Astral/StaminaPotion4.png']
-
 
 altar1_images = ['Images/Runecrafting/Astral/Altar1.png']
 altar2_images = ['Images/Runecrafting/Astral/Altar2.png']
@@ -89,8 +95,62 @@ fix_pouches_images = ['Images/Runecrafting/Astral/Repair.png']
 
 low_health_images = ['Images/Runecrafting/Astral/LowHealth.png']
 lobster_images = ['Images/Runecrafting/Astral/Lobster.png']
+home_teleport_images = ['Images/Runecrafting/Astral/HomeTeleport.png']
+pool_images = ['Images/Runecrafting/Astral/Pool.png']
 
 astral_altar = [1035, 323]
+
+
+def open_bank1():
+    Common.watch_click_image(bank1_images, 0.7, 'Click bank booth and look for bank options', False,
+                             1, 10, current_step_region=Common.Main_game_screen_region,
+                             next_step_image_paths=Banking.bank_withdraw_note_images, next_step_confidence=0.4,
+                             next_step_region=Common.Bank_bottom_options_region)
+
+
+def click_inventory_tab():
+    Common.watch_click_image(inventory_tab_images, 0.7, 'Switch back to inventory tab', False,
+                             2, 10, None, Common.Inventory_bar_region)
+
+
+def go_to_bank_from_teleport():
+    Common.watch_click_image(bank2_images, 0.7, 'Click bank booth 2 while looking for bank booth 1',
+                             False, 7, 10, current_step_region=Common.Main_game_screen_region,
+                             next_step_image_paths=bank1_images,
+                             next_step_region=Common.Main_game_screen_region,
+                             next_step_confidence=0.7)
+    open_bank1()
+
+
+def cast_lunar_teleport():
+    Common.watch_click_image(lunar_spellbook_images, 0.7, 'Open spellbook while looking for teleport',
+                             False, 2, 10, teleport_images,
+                             Common.Inventory_bar_region, Common.Inventory_region)
+
+    Common.watch_click_image(teleport_images, 0.7, 'Click teleport', False, 5,
+                             10, None, Common.Inventory_region)
+
+
+def pouch_astral_runes():
+    Common.watch_click_image(astral_rune_images, 0.7, 'Click astrals in inventory',
+                             False, 1, 10, None, Common.Inventory_region)
+
+    Common.watch_click_image(rune_pouch_images, 0.7, 'Click rune pouch in inventory',
+                             False, 1, 10, None, Common.Inventory_region)
+
+
+def heal_hp():
+    is_hp_low = Common.is_image_on_screen(low_health_images, 0.6, 0, 'Check if health is low',
+                                          Common.Minimap_vitals_region)
+    # If HP is low grab lobster and heal
+    if is_hp_low:
+        Common.watch_click_image(lobster_images, 0.7, 'Click bank lobster', True,
+                                 1, 10, None, Common.Bank_region)
+        Banking.close_bank()
+        Common.watch_click_image(lobster_images, 0.7, 'Click lobster to heal', False,
+                                 2, 10, None, Common.Inventory_region)
+
+        open_bank1()
 
 
 def check_repair_pouches(iteration):
@@ -128,8 +188,7 @@ def craft_astrals():
         item_count = int(item_count_string)
         loops_till_done = round(item_count / runes_crafted_per_run)
 
-        Common.watch_click_image(bank1_images, 0.7, 'Click bank booth', False,
-                                 1, 10, None, Common.Main_game_screen_region)
+        open_bank1()
 
         Common.watch_click_image(tiara_bank_tab_images, 0.7, 'Click tiara bank tab', False,
                                  1, 10, None, Common.Top_half_game_screen_region)
@@ -210,30 +269,35 @@ def craft_astrals():
             Common.watch_click_image(altar2_images, 0.6, 'Click altar 2 to craft more runes', False,
                                      3, 10, None, Common.Main_game_screen_region)
 
-            Common.watch_click_image(lunar_spellbook_images, 0.7, 'Open spellbook while looking for teleport',
-                                     False, 2, 10, teleport_images,
-                                     Common.Inventory_bar_region, Common.Inventory_region)
-
-            Common.watch_click_image(teleport_images, 0.7, 'Click teleport', False, 5,
-                                     10, None, Common.Inventory_region)
-
+            cast_lunar_teleport()
             check_repair_pouches(x)
+            click_inventory_tab()
+            pouch_astral_runes()
+            go_to_bank_from_teleport()
 
-            Common.watch_click_image(inventory_tab_images, 0.7, 'Switch back to inventory tab', False,
-                                     2, 10, None, Common.Inventory_bar_region)
-
-            Common.watch_click_image(bank2_images, 0.7, 'Click bank booth 2 while looking for bank booth 1',
-                                     False, 7, 10, current_step_region=Common.Main_game_screen_region,
-                                     next_step_image_paths=bank1_images,
-                                     next_step_region=Common.Main_game_screen_region,
-                                     next_step_confidence=0.7)
-            Common.watch_click_image(bank1_images, 0.7, 'Click bank booth 1', False, 2,
-                                     10, None, Common.Main_game_screen_region)
-
-            Common.watch_click_image(astral_rune_images, 0.7, 'Click astrals in inventory to deposit them',
-                                     False, 1, 10, None, Common.Inventory_region)
-
+            # Display timer here so it's not inflated by stamina restore run
             Display.stop_timer(start_time, loops_till_done)
+
+            # Teleport to house and restore stamina every couple of runs
+            if x % 6 == 0:
+                Common.watch_click_image(home_teleport_images, 0.7, 'Withdraw home teleport tablet', False,
+                                         1, 10, None,
+                                         Common.Bank_region)
+
+                Banking.close_bank()
+
+                Common.watch_click_image(home_teleport_images, 0.7, 'Use home teleport tablet', False,
+                                         7, 10, None,
+                                         Common.Inventory_region)
+
+                # Use stamina pool
+                Common.watch_click_image(pool_images, 0.7, 'Use pool to restore stamina', False,
+                                         7, 10, None,
+                                         Common.Main_game_screen_region)
+                cast_lunar_teleport()
+                click_inventory_tab()
+                go_to_bank_from_teleport()
+
     # Image not found error, stop loop and print message
     except ValueError as error:
         print(error, 'Stopping process')
